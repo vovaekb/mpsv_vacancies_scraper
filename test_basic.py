940b6477cb35c286b9167af2357e7f19c557c6aa
Vladimir Privalov <vladprivalov1990@gmail.com>
2020-08-06 14:17:58 +0200

    Add tests

---
+++ test_basic.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+from app import app
+
+class BasicTests(unittest.TestCase):
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    def tearDown(self):
+        pass
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
